29707e5ef4ba56eb7c13806acb98daf3c7559e04
Hariharan2293 <112700316+Hariharan2293@users.noreply.github.com>
2022-09-20 09:40:02 +0530

    Add files via upload

---
+++ d1program7.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+// Returns true if n is perfect
+bool isPerfect(long long int n)
+{
+	// To store sum of divisors
+	long long int sum = 1;
+
+	// Find all divisors and add them
+	for (long long int i=2; i*i<=n; i++)
+	{
+		if (n%i==0)
+		{
+			if(i*i!=n)
+				sum = sum + i + n/i;
+			else
+				sum=sum+i;
+		}
+	}
+	// If sum of divisors is equal to
+	// n, then n is a perfect number
+	if (sum == n && n != 1)
+		return true;
+
+	return false;
+}
+
+// Driver program
+int main()
+{
+	int z;
+	cout<<"ENTER:"<<z;
+	cin>>z;
+	cout << "all perfect numbers in\n";
+	for (int n =1; n<=z; n++)
+		if (isPerfect(n))
+			cout << n << " is a perfect number\n";
+
+	return 0;
+}
+
