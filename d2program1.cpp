0125ae8e71976387a277ee2d4e86c39e7835dcb5
Hariharan2293 <112700316+Hariharan2293@users.noreply.github.com>
2022-09-21 13:39:25 +0530

    Add files via upload

---
+++ d2program1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r,l,b,s,p,h;
+	cout<<"ENTER RADIUS OF CIRCLE:";
+	cin>>r;
+	cout<<"ENTER lenght and b:";
+	cin>>l;
+	cin>>b;
+	cout<<"ENTER FOR SQUARE:";
+	cin>>s;
+	cout<<"ENTER BASE AND HEIGHT:";
+	cin>>p;
+	cin>>h;
+	cout<<"THE AREA OF CIRCLE:"<<3.14*r*r<<endl;
+	cout<<"THE AREA OF RECTANGLE:"<<l*b<<endl;
+	cout<<"THE AREA OF SQUARE:"<<s*s<<endl;
+	cout<<"THE AREA OF TRIANGLE:"<<0.5*p*h<<endl;
+return 0;
+}
+
